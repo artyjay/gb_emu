ca4eaf9deffc2d35e5387e94cbc8f8d45becc265
Robert Johnson <robert.johnson@v-nova.com>
2019-01-03 11:08:47 +0000

    Re-organise emulator, reducing complexity of project structure.

--- src/desktop/main.cpp
+++ src/desktop/main.cpp
@@ -1,9 +1,157 @@
-#include <gbe.h>
+#include <gbhw.h>
+#include <SDL.h>
+#include <stdio.h>
+#include <type_traits>
+
+#ifdef WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
+
+//--------------------------------------------------------------------------
+
+namespace
+{
+	template<typename... Args>
+	inline void log_message(const char* format, Args... parameters)
+	{
+		static char buffer[4096];
+
+		int32_t len = sprintf(buffer, format, std::forward<Args>(parameters)...);
+
+		fwrite(buffer, sizeof(char), len, stdout);
+
+#ifdef WIN32
+		OutputDebugString(buffer);
+#endif
+	}
+
+	void hw_log_callback(void* userdata, gbhw_log_level_t level, const char* msg)
+	{
+		log_message(msg);
+	}
+}
+//------------------------------------------------------------------------------
 
 int main(int argc, char* args[])
 {
-	gbe_context_t context = gbe_create();
-	int32_t	res = gbe_main_loop(context);
-	gbe_destroy(context);
-	return res;
+	log_message("Welcome to a Gameboy emulator\n");
+
+	if (argc != 2)
+	{
+		log_message("Invalid command line specified\n");
+		log_message("\tUsage: EXE <ROM PATH>\n");
+		return false;
+	}
+
+	// Create hardware
+	gbhw_settings_t settings	= {0};
+	settings.log_callback		= hw_log_callback;
+	settings.log_level			= l_debug;
+	settings.log_userdata		= nullptr;
+	settings.rom_path			= args[1];
+
+	gbhw_context_t hardware		= nullptr;
+
+	if (gbhw_create(&settings, &hardware) != e_success)
+		return -1;
+
+	// Grab screen details and setup SDL.
+	uint32_t screenWidth = 0;
+	uint32_t screenHeight = 0;
+	uint32_t screenScale = 4;
+
+	if (gbhw_get_screen_resolution(hardware, &screenWidth, &screenHeight) != e_success)
+		return -1;
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window*	window = SDL_CreateWindow("Gameboy Emulator",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		screenWidth * screenScale,
+		screenHeight * screenScale,
+		SDL_WINDOW_SHOWN);
+
+	if (!window)
+	{
+		log_message("Failed to create window, error: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	if (!renderer)
+	{
+		log_message("Failed to create renderer, error: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	SDL_Texture* texture = SDL_CreateTexture(renderer,
+		SDL_PIXELFORMAT_BGRX8888,
+		SDL_TEXTUREACCESS_STREAMING,
+		screenWidth,
+		screenHeight);
+
+	if (!texture)
+	{
+		log_message("Failed to create texture, error: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	// Enter main-loop.
+	SDL_Event e;
+	bool bQuit = false;
+
+	while (!bQuit)
+	{
+		while (SDL_PollEvent(&e) != 0)
+		{
+			//User requests quit
+			if (e.type == SDL_QUIT)
+			{
+				bQuit = true;
+			}
+			else if ((e.type == SDL_KEYDOWN) || (e.type == SDL_KEYUP))
+			{
+				gbhw_button_state_t state = (e.type == SDL_KEYDOWN) ? button_pressed : button_released;
+
+				switch (e.key.keysym.sym)
+				{
+				case SDLK_LEFT:			gbhw_set_button_state(hardware, button_dpad_left, state); break;
+				case SDLK_RIGHT:		gbhw_set_button_state(hardware, button_dpad_right, state); break;
+				case SDLK_UP:			gbhw_set_button_state(hardware, button_dpad_up, state); break;
+				case SDLK_DOWN:			gbhw_set_button_state(hardware, button_dpad_down, state); break;
+				case SDLK_RETURN:		gbhw_set_button_state(hardware, button_start, state); break;
+				case SDLK_BACKSPACE:	gbhw_set_button_state(hardware, button_select, state); break;
+				case SDLK_SPACE:		gbhw_set_button_state(hardware, button_a, state); break;
+				case SDLK_b:			gbhw_set_button_state(hardware, button_b, state); break;
+				default: break;
+				}
+			}
+		}
+
+		// Tick hardware until there's a vsync.
+		gbhw_step(hardware, step_vsync);
+
+		// @todo: Rate limit.
+
+		const uint8_t* screen = nullptr;
+		if (gbhw_get_screen(hardware, &screen) != e_success)
+			return -1;
+
+		// Screen data is generated as XRGB8 data packed into uint32_t.
+		SDL_UpdateTexture(texture, nullptr, screen, screenWidth * sizeof(uint32_t));
+		SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
 }
+
+//------------------------------------------------------------------------------
